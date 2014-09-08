4ce995f960942ac2388aac6146462aa4cfe89bc0
Navya Nidhanapati <navyan@boardboysat-lm.corp.yahoo.com>
2014-09-07 18:13:43 -0700

    Initial Commit

---
+++ TIPC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TIPC
+//
+//  Created by Navya Nidhanapati on 9/7/14.
+//  Copyright (c) 2014 Navya Nidhanapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
